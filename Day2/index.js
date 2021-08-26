2129258c121e7887979de752509351ca64dfb18e
Shivam Kumar <70000974+Shivam2883@users.noreply.github.com>
2021-08-26 13:39:56 +0530

    Add files via upload

---
+++ Day2/index.js
@@ -0,0 +1,90 @@
+
+
+let Food = [
+    {
+        name: "Chicken Chilli",
+        price: 185,
+        quantity: 1
+    },
+    {
+        name: "Paneer Chilli",
+        price: 140,
+        quantity: 1
+    },
+    {
+        name: "Masala Dosa",
+        price: 120,
+        quantity: 2
+    },
+    {
+        name: "Idli Sambhar",
+        price: 100,
+        quantity: 4
+    },
+    {
+        name: "Pasta",
+        price: 80,
+        quantity: 1
+    }
+]
+
+
+for (let num = 0; num < Food.length; num++) {
+    console.log("Item: "+Food[num].name);
+    console.log("Price: "+Food[num].price);
+    console.log("Quantity: "+Food[num].quantity);
+    console.log("\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
